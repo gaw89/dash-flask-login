cd9c67ea821d742bc51e8020e283a2ee2be8e0cc
Jeroen de Vries <jeroendevries@runbox.eu>
2022-02-11 11:02:38 +0100

    Update main repo to version as on Pypi

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+# Metadata goes in setuf.cfg
+setup(
+    name="dash_flask_login",
+    install_requires=[
+        "dash >= 1.9.0",
+        "flask-login >=0.5.0",
+    ],
+)
--- src/dash_flask_login/__init__.py
+++ src/dash_flask_login/__init__.py
@@ -1,3 +1,4 @@
 from __future__ import absolute_import
 from .flask_login_auth import FlaskLoginAuth
-from .version import __version__  # noqa: F401
+
+__version__ = "0.0.7"
